e0c441bb11c4b84698b93ea3c9ceda886de60de0
Vicky Jaiswal <vjaiswal@hcg.com>
2025-12-01 14:17:58 -0600

    fix: Add missing Separator component

---
+++ frontend/components/ui/separator.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "horizontal" | "vertical"
+  decorative?: boolean
+}
+
+const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
+  ({ className, orientation = "horizontal", ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "shrink-0 bg-slate-200 dark:bg-slate-800",
+        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        className
+      )}
+      {...props}
+    />
+  )
+)
+Separator.displayName = "Separator"
+
+export { Separator }
